ef853d3b3050d1400d4629263c88be7ec5466cbc
neosloth <neosloth@posteo.net>
2018-08-08 16:38:46 -0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,8 @@
+;; This file replaces itself with the config from init.org
+
+(require 'org)
+(find-file (concat user-emacs-directory "init.org"))
+
+(org-babel-tangle)
+
+(load-file (concat user-emacs-directory "init.el"))
